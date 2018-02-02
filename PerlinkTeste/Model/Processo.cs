1ae1dae29c17a5a797f5a2eccc63c1ab4113bca3
Tássyo Santana <tassyo.santana@hotmail.com>
2018-02-02 11:40:21 -0200

    Inicial

---
+++ PerlinkTeste/Model/Processo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PerlinkTeste.Model
+{
+    public class Processo
+    {
+        public int Id { get; set; }
+        public string Numero { get; set; }
+        public DateTime DataInicio { get; set; }
+        public Decimal Valor { get; set; }
+        public bool Status { get; set; }
+        public int EstadoId { get; set; }
+    }
+}
